805de99453416529ea70edd3ebb038ab144c51c8
ecormaksin <truth_jp_4133@yahoo.co.jp>
2021-09-22 21:57:25 +0900

    first commit

---
+++ src/main/java/com/example/blazepersistencesample/BlazePersistenceSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.blazepersistencesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazePersistenceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlazePersistenceSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blazepersistencesample/BlazePersistenceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blazepersistencesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlazePersistenceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
